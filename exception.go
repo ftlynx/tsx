8732da38d06dba2bef07c22cc165302883c7f485
fengtao <fengtao@fiture.com>
2021-01-12 10:41:13 +0800

    add exception

--- exception.go
+++ exception.go
@@ -2,7 +2,6 @@ package tsx
 
 import (
 	"fmt"
-	"net/http"
 	"runtime"
 	"time"
 )
@@ -16,14 +15,15 @@ type Exception struct {
 }
 
 func (e *Exception) Error() string {
-	return e.ErrMsg
+	return fmt.Sprintf("%s:%d %s", e.Filename, e.Line, e.ErrMsg)
 }
 
-func Err(err error, errCode ...int) error {
+//用于获取错误的位置及设置errCode
+func Error(err error, errCode ...int) error {
 	if err == nil {
 		return err
 	}
-	code := http.StatusInternalServerError
+	code := 0
 	if len(errCode) > 0 {
 		code = errCode[0]
 	}
