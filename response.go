75d88911f9a9bdc6eb9426d180d33b0760ed292d
fengtao <fengtao@fiture.com>
2021-08-18 15:50:23 +0800

    fix

--- response.go
+++ response.go
@@ -131,7 +131,7 @@ func (p *QueryPaging) defaultValue() {
 func ResponseParse(result *Response) request.ResultParse {
 	return func(resp request.HttpResponse) error {
 		if resp.Code != http.StatusOK {
-			_ = json.Unmarshal(resp.Content, result)
+			result.HttpCode = resp.Code
 			return fmt.Errorf("opsx response http code not 200. is %d", resp.Code)
 		}
 		return json.Unmarshal(resp.Content, result)
